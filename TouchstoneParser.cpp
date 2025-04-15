dcca085c67c9be9a7b33a7f105bcced3b2ff4bc7
KVRTeJ <bb.sssss@inbox.ru>
2025-04-16 02:07:04 +0700

    Реализован парсинг файла. Теперь m_data полностью заполняется числами с файла

--- TouchstoneParser.cpp
+++ TouchstoneParser.cpp
@@ -11,11 +11,46 @@ bool TouchstoneParser::parse() {
         return false;
     }
 
+    ParseData currentData;
     QTextStream in(&file);
     while (!in.atEnd()) {
         QString line = in.readLine();
 
-        qDebug() << line;
+        if (line.isEmpty()) {
+            continue;
+        }
+
+        if(line[0] == '#' || line[0] == '!')
+            continue;
+
+        QStringList parts = line.split(' ', Qt::SkipEmptyParts);
+        if (parts.size() != 3) {
+            qDebug() << "uncorrect format file: has" << parts.size() << "in line:" << line;
+            file.close();
+            return false;
+        }
+
+        bool convertFrequency, convertReal, convertImag;
+        double frequency = parts[0].toDouble(&convertFrequency);
+        double real = parts[1].toDouble(&convertReal);
+        double imag = parts[2].toDouble(&convertImag);
+
+        if(!(convertFrequency && convertReal && convertImag)) {
+            qDebug() << "uncorrect format file: cant convert to double" << "in line:" << line;
+            file.close();
+            return false;
+        }
+
+        currentData.frequency = frequency;
+        currentData.real = real;
+        currentData.imag = imag;
+
+        m_data.push_back(currentData);
     }
 
+    for(auto it = m_data.begin(); it != m_data.end(); ++it) {
+        qDebug() << (*it).frequency << (*it).real << (*it).imag;
+    }
+    file.close();
+    return true;
 }
--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 int main(int argc, char *argv[]) {
     TouchstoneParser boo;
+
     boo.setFileName("C:\\Users\\dmitriy.filimonov\\Downloads\\S11.S1P");
     boo.parse();
+
     return -1;
 }
