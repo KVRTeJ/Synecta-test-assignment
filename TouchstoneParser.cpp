d5ebc6f1cfc6816f05a73a9a13e0f1a46e2bfa6f
KVRTeJ <bb.sssss@inbox.ru>
2025-04-16 03:39:34 +0700

    Рефакторинг

--- TouchstoneParser.cpp
+++ TouchstoneParser.cpp
@@ -7,26 +7,20 @@ bool TouchstoneParser::parse() {
     QFile file(m_fileName);
 
     if(!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "cant open file" << m_fileName;
+        qDebug() << "cant open file" << m_fileName; //TODO: to emit
         return false;
     }
 
-    ParseData currentData;
     QTextStream in(&file);
     while (!in.atEnd()) {
         QString line = in.readLine();
 
-        if (line.isEmpty()) {
-            continue;
-        }
-
-        if(line[0] == '#' || line[0] == '!')
+        if(line.isEmpty() || line[0] == '#' || line[0] == '!')
             continue;
 
         QStringList parts = line.split(' ', Qt::SkipEmptyParts);
         if (parts.size() != 3) {
-            qDebug() << "uncorrect format file: has" << parts.size() << "in line:" << line;
-            file.close();
+            qDebug() << "uncorrect format file: has" << parts.size() << "in line:" << line; //TODO: to emit
             return false;
         }
 
@@ -35,22 +29,17 @@ bool TouchstoneParser::parse() {
         double real = parts[1].toDouble(&convertReal);
         double imag = parts[2].toDouble(&convertImag);
 
-        if(!(convertFrequency && convertReal && convertImag)) {
-            qDebug() << "uncorrect format file: cant convert to double" << "in line:" << line;
-            file.close();
+        if(!convertFrequency || !convertReal || !convertImag) {
+            qDebug() << "uncorrect format file: cant convert to double" << "in line:" << line; //TODO: to emit
             return false;
         }
 
-        currentData.frequency = frequency;
-        currentData.real = real;
-        currentData.imag = imag;
+        m_data.push_back({frequency,
+                          real,
+                          imag});
 
-        m_data.push_back(currentData);
     }
 
-    for(auto it = m_data.begin(); it != m_data.end(); ++it) {
-        qDebug() << (*it).frequency << (*it).real << (*it).imag;
-    }
     file.close();
     return true;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <QDebug>
 
 #include "TouchstoneParser.h"
 
@@ -7,6 +8,12 @@ int main(int argc, char *argv[]) {
 
     boo.setFileName("C:\\Users\\dmitriy.filimonov\\Downloads\\S11.S1P");
     boo.parse();
+
+    auto parsed = boo.getParsedData();
+    qDebug() << parsed.size();
+    for(auto it = parsed.begin(); it != parsed.end(); ++it) {
+        qDebug() << (*it).frequency << (*it).real << (*it).imag;
+    }
 
     return -1;
 }
